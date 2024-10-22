09cf00eb3daef11ae4682e8644023555ef302eab
vinci <74002397+supervincii@users.noreply.github.com>
2024-10-22 21:52:24 +0800

    update emacs config with config for C programming + misc.

--- init.el
+++ init.el
@@ -1,35 +1,45 @@
-(setq ring-bell-function 'ignore)
-(setq inhibit-splash-screen t)
+;; Speed up initial startup
+(setq
+ gc-cons-threshold 402653184
+ gc-cons-percentage 0.6)
+
+(setq
+ ring-bell-function 'ignore
+ inhibit-splash-screen t
+ display-line-numbers-type t
+ make-backup-files nil
+ auto-save-default nil
+ initial-scratch-message nil
+ initial-major-mode 'eshell-mode
+ recentf-max-saved-items 50
+ confirm-kill-emacs 'y-or-n-p
+ password-cache t
+ password-cache-expiry 3600)
+
+(setq-default
+ fill-column 80
+ line-spacing 5
+ indent-tabs-mode nil
+ cursor-in-non-selected-windows nil)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-
+(blink-cursor-mode -1)
+(global-hl-line-mode)
 (global-display-line-numbers-mode t)
-(setq display-line-numbers-type 'relative)
 (column-number-mode)
-
-(setq-default fill-column 80)
-(global-display-fill-column-indicator-mode 1)
-
-(set-face-attribute 'default nil :font "Iosevka-14")
-(setq-default line-spacing 5)
-
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq modus-themes-mode-line '(borderless))
-(setq modus-themes-region '(bg-only))
-(setq modus-themes-bold-constructs t)
-(load-theme 'modus-vivendi t)
-
+(recentf-mode)
 (global-visual-line-mode t)
 (delete-selection-mode t)
 (electric-pair-mode t)
-(setq-default indent-tabs-mode nil)
+(global-display-fill-column-indicator-mode 1)
 
-;; Autosaves & Backups
-(setq make-backup-files nil)
-(setq auto-save-default nil)
+;; Font setting
+(set-face-attribute 'default nil :font "Iosevka-14")
+
+;; Default maximize the screen
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; Packages
 (require 'package)
@@ -37,7 +47,12 @@
 
 (package-initialize)
 
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
+
+;; theme
+(use-package kanagawa-themes
+  :config (load-theme 'kanagawa-wave t))
 
 ;; general editing config
 
@@ -57,7 +72,52 @@
 
 ;; TODO
 ;; C, Rust, Python, Go, ASM, JS, TS, programming config
-;; LSP, tree-sitter
+
+;; C config
+(defun vinci/c-setup ()
+  "C programming configuration"
+  (c-set-style "linux")
+  (setq-local tab-width 4)
+  (setq-local c-basic-offset 4))
+
+(add-hook 'c-mode-hook 'vinci/c-setup)
+(add-hook 'c-ts-mode-hook 'vinci/c-setup)
+(add-hook 'c++-mode-hook 'vinci/c-setup)
+(add-hook 'c++-ts-mode-hook 'vinci/c-setup)
+
+
+;; LSP
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook
+  ((c-ts-mode . lsp)
+   (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+;; Snippets
+(use-package yasnippet
+  :config
+  (yas-reload-all)
+  :hook
+  ((lsp-mode . yas-minor-mode)))
+
+;; Installing a tree-sitter parser
+;; Run "M-x treesit-install-language-grammar", then follow the instructions.
+;; Tree-sitter config
+(defun run-non-ts-hooks ()
+  (let ((major-name (symbol-name major-mode)))
+    (when (string-match-p ".*-ts-mode" major-name)
+      (run-hooks (intern (concat (replace-regexp-in-string "-ts" "" major-name) "-hook"))))))
+
+(add-hook 'prog-mode-hook 'run-non-ts-hooks)
 
 ;; magit
+(use-package magit)
+
 ;; which-key
+(use-package which-key
+  :config (which-key-mode))
