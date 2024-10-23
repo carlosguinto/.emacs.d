5e3bfb1dc33e4d2d1f46e42f8a65c85464581f4d
vinci <74002397+supervincii@users.noreply.github.com>
2024-10-23 18:03:17 +0800

    update emacs config

--- init.el
+++ init.el
@@ -50,19 +50,24 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-;; theme
-(use-package kanagawa-themes
-  :config (load-theme 'kanagawa-wave t))
+;; Theme
+(use-package timu-caribbean-theme
+  :config (load-theme 'timu-caribbean t))
 
-;; general editing config
+;; General Editing
 
 (use-package rainbow-delimiters
   :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
-;; org-mode packages
+;; Org Config
 
 (require 'org-bullets)
+
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-hook 'org-mode-hook 'org-indent-mode)
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN PROGRESS" "|" "DONE")))
 
 ;; To add
 ;; org toc
@@ -70,15 +75,19 @@
 ;; org roam
 
 
+;; Software Development Packages
 ;; TODO
-;; C, Rust, Python, Go, ASM, JS, TS, programming config
+;; Rust, Python, Go, ASM, JS, TS, programming config
 
 ;; C config
 (defun vinci/c-setup ()
   "C programming configuration"
-  (c-set-style "linux")
-  (setq-local tab-width 4)
-  (setq-local c-basic-offset 4))
+  (setq-local c-default-style "linux"
+              tab-width 4
+              c-basic-offset 4
+              indent-tabs-mode nil)
+  (setq-local c-ts-mode-indent-style "linux"
+              c-ts-mode-indent-offset 4))
 
 (add-hook 'c-mode-hook 'vinci/c-setup)
 (add-hook 'c-ts-mode-hook 'vinci/c-setup)
@@ -108,6 +117,11 @@
 ;; Installing a tree-sitter parser
 ;; Run "M-x treesit-install-language-grammar", then follow the instructions.
 ;; Tree-sitter config
+(setq major-mode-remap-alist
+      '((c-mode . c-ts-mode)
+        (c++-mode . c++-ts-mode)))
+
+;; Custom function to run hooks of non-ts mode counterparts.
 (defun run-non-ts-hooks ()
   (let ((major-name (symbol-name major-mode)))
     (when (string-match-p ".*-ts-mode" major-name)
@@ -115,9 +129,26 @@
 
 (add-hook 'prog-mode-hook 'run-non-ts-hooks)
 
-;; magit
+;; Version Control
+;; Magit
 (use-package magit)
 
-;; which-key
+;; Helper Packages
 (use-package which-key
   :config (which-key-mode))
+
+;; Minibuffer Packages
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(timu-caribbean-theme yasnippet which-key rainbow-delimiters org-bullets magit lsp-ui kanagawa-themes doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
